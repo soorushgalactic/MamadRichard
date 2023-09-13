e213ea06b70344183434e9140e05da0de2ef19ea
SOORUSHGALACTIC <soorushgalactic@gmail.com>
2023-09-13 17:32:50 +0330

    commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 let prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
+  if (currentScrollPos < 60) {
     document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = "-64px";
   }
-  prevScrollpos = currentScrollPos;
 };
+// window.onscroll = function () {
+//   let currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-64px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// };
